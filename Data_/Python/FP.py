b4d61027f9aa359242e2386f28ebb15527c1f7a9
Prerna <mhatreprerna800@gmail.com>
2026-07-17 16:11:28 +0530

    python

--- Data_/Python/FP.py
+++ Data_/Python/FP.py
@@ -1,1 +1,23 @@
 print("Hello World")
+print(" first program ")
+
+print(""" nsvj
+      nvjchvi
+      njxhvi
+      """)
+
+print("welcome" \
+"\n to " \
+"\n Python")
+
+print(''' hello
+      world
+      !!
+      ''')
+
+print("hello \nWelocme \nHIII!!")
+
+print('hello puthon')
+
+print('it\'s ggoing to rain')
+print('it's ggoing to rain')
