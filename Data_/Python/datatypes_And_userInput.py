43b16d6bbe36bb3eee8fde1f40b8b9c54bb010f3
Prerna <mhatreprerna800@gmail.com>
2026-07-17 17:05:48 +0530

    datatypes and user input - int , string, float, evaluate

---
+++ Data_/Python/datatypes_And_userInput.py
@@ -0,0 +1,39 @@
+"""DataTypes
+Text - String (str)
+Numeric- integer (int)
+          floating point (float)
+          compplex ()
+Sequence - list
+            tuple
+            range
+Mapping  - Dictionaries (dict)
+set  - set
+        frozenset
+Boolean - bool
+Binary - bytes
+         bytearray
+         memoryview"""
+
+'''
+User Input
+input given by user
+default its string type
+
+
+'''
+# name=input("enter name : ")
+# print(name)
+
+# # enter name : a
+# # a
+
+# #integer
+# age= int(input("enter age : "))
+# print(age)
+
+# height=float(input("Enter height  : "))
+# print(height)
+
+exp1=eval(input("Enter any equation here : "))
+print(exp1)
+
