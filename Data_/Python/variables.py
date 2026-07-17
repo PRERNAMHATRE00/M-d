11cec558a63b4895b995d27910ff738c1ee80e30
Prerna <mhatreprerna800@gmail.com>
2026-07-17 16:34:46 +0530

    variables and rules

---
+++ Data_/Python/variables.py
@@ -0,0 +1,23 @@
+greet="Welcome"
+print(greet )
+
+#rule 1  case sensetive
+# Name= "Ash"
+# print(name)
+
+NAME="Ash"
+print(NAME)
+
+# rule 2 not use space in between while creating variable
+#name 1 = "a"
+name_1="s"
+name1="d"
+nameN1="f"
+
+# rule 3  variable name should nevers start with number or special symbol
+# 1num=2
+# @num=4
+# %num=5
+Num1=2
+num1=3
+num@=4
