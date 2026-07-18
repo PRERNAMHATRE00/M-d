5f0ecaa2dbcf5e63a2d4eeae251806b174816fa6
Prerna <mhatreprerna800@gmail.com>
2026-07-18 23:02:16 +0530

    problem set 1

---
+++ Data_/Python/problem_solving.py
@@ -0,0 +1,65 @@
+# display person (name, age, address in 3 diff lines)
+# person_name="Sia"
+# person_age=26
+# person_address="Green Place, near Post office, Malad(west)"
+# print(person_name)
+# print(person_age)
+# print(person_address)
+
+
+#swap two numbers
+
+# num1=10
+# num2=30
+# print("before num1 ",num1)
+# print("before num2 ",num2)
+# swap=num2
+# num2=num1
+# num1=swap
+# print("after num1 ",num1)
+# print("after num2 ",num2)
+
+
+#METHD 2
+# a= 10
+# b=40
+# a,b=b,a
+# print(b)
+# print(a)
+
+#convert float to int 
+
+# num=13.5
+# print("float :",num)
+# print(type(num))
+# num=int(num)
+# print("Int",num)
+# print(type(num))
+
+#convert int to float
+# num=10
+# print(num)
+# print(type(num))
+# num= float(num)
+# print("Float",num)
+# print(type(num))
+
+
+#take details from  student for ID card and print in diff lines
+#print("Enter ID card Details")
+# Name=input("\nEnter your name : \n")
+# age=int(input("Enter your age :\n "))
+# address=input("Enter your address:\n ")
+# BloodG= input("Enter blood Group :\n ")
+#print("Enter ID card Details")
+# print(Name )
+# print(age)
+# print(address )
+# print(BloodG )
+
+#user i/p as int and convert to float
+# age = int(input("Enter age : "))
+# print(age)
+# age=float(age)
+# print(age)
+# print(type(age))
