03bb6a60bcb089bae5cdc57387f14a3e8a7b2c4e
Prerna <mhatreprerna800@gmail.com>
2026-07-19 18:45:31 +0530

    Operators and operands  7 types (Arithmethic,Comparison,Logical,Assignment,Identity,Bitwise,Membership)

---
+++ Data_/Python/operators_&_operands.py
@@ -0,0 +1,86 @@
+#Arithmethic
+# a=20
+# b=15
+
+# print("Addition : ",a+b)
+# print("Subtraction : ",a-b)
+# print("Multiplication : ", 5 *2)
+# print("Division : ",40/5)
+# print("Exponention : ",2**3)
+# print("Floor Division : ",12//3)
+# print("Modulus : ",8 % 3)
+
+#comparison
+# print("Greater than",6 > 3)
+# print("Less than",6 < 3)
+# print( "Equal to",3 ==4 )
+# print("Equal to ",3 == 3)
+# print( "Not equal to",3 != 3)
+# print("Not equal to ",3 != 4)
+# print("greater than equal to",3>=3)
+# print ("Greater than equal to",3>=6)
+# print("less than equal",3<=3)
+# print("less than equal to ",3<=4)
+
+#logical 
+# print(3>2 and 7>4)
+# print(3<2 and 7>4)
+# print(3>2 or 7>4)
+# print(3>2 or 7<4)
+# print(3<2 or 7<4)
+# print(not(3>2 and 7>4))
+# print(not(3>2 and 7<4))
+
+#Assignment
+# a=10
+# print(a)
+# b=a # a value is assigned to b
+# print(b)
+# a +=2 #a is incremented by 2 or 2 is added to value of a 
+# print(a)
+# b-=1 # b is decremented or 1 is subtracted from values of b
+# print(b)
+# a*=5 #value of a is multiplied by 5
+# print(a)
+
+#Identity
+# a=102
+# b="102"
+# print("a is b",a is b)
+# c=222
+# d=222
+# print("c is d  ",c is d)
+# print( " is not d  ",a is not d)
+# print("c is not d",c is not d)
+
+#Bitwise
+#AND
+# print(bin(10))
+
+# a=10
+# b=8
+# print(a & b)
+
+#OR
+# a=10
+# b=7
+# print(a | b)
+
+#XOR
+# a=10
+# b=5
+# print(a ^ b)
+
+#zero fill left shift
+# print(10 >> 1)
+
+# #zero fill right shift
+# print(10 << 1)
+
+
+#Membership
+# a="Welcome"
+# print("l" in a)
+# print("y" in a)
+# print("c" not in a)
+# print("q" not in a)
