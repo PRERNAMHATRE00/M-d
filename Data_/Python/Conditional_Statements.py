1d358bac9be88b3e083262604a151a1da65e8c47
Prerna <mhatreprerna800@gmail.com>
2026-07-19 19:34:12 +0530

    Conditional statment types - if, if-else,Nested if, short hand if , short hand if-else

---
+++ Data_/Python/Conditional_Statements.py
@@ -0,0 +1,50 @@
+#if the statement
+# marks=82
+# if marks>=80 :
+#     print("Marks are above 80")
+
+# print("Thank You !!")
+
+#if else statement
+# marks=87
+# if marks>=90:
+#     print("Marks are abve 90")
+# else :
+#     print("Marks below 90")
+
+# print("Thank You !!")
+
+#if-elif-else
+# num=int(input("Enter a any number between 40 to 100 : "))
+# if num >=90:
+#     print("Number Between 90-100")
+# elif num >=80:
+#     print("Number Between 80-90")
+# elif num>=70:
+#     print("Number Between 70-80")
+# elif num>=60:
+#     print("Number Between 60-70")
+# elif num>=50:
+#     print("Number Between 50-60")
+# elif num>40:
+#     print("Number Between 40-50")
+# else:
+#     print("Number is between 0 - 30")
+
+#Nested if 
+# marks=70
+# if marks>=80:
+#     print("First Class ")
+#     if marks>=95:
+#         print("Topper")
+
+# else:
+#     print("Passed")
+
+#short hand if
+# marks=80
+# if marks>=80: print("First class")
+
+#short hand if els
+# marks=50
+# print("first class") if marks>=90 else print("Just Passed")
