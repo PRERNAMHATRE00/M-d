1fbda9b29610eb4562c454c2475b0b4ae20d479c
Prerna <mhatreprerna800@gmail.com>
2026-07-21 22:58:05 +0530

    For loop, While loop, while True

---
+++ Data_/Python/Loops_&_Types.py
@@ -0,0 +1,76 @@
+#For loop
+# for i in range(1,8,2):
+#     print("HEY!!",i)
+#for i in range(1,8):
+#     print("HEY!!",i)
+
+#Table of 9
+# n=int(input("enter number for table "))
+# for i in range(1,20,2):
+#     print(n,"x",i,"=",n*i)
+
+#While Loop
+# n=0
+# while n<=10:
+#     print("Hi",n)
+#     n=n+2 #n +=1
+
+#TABLE
+# n=1
+# v=5
+# while n<=10:
+#     print(v,"X",n," = ",v*n)
+#     n+=1
+
+#while True
+# while True:
+#     num1=int(input("num1 ="))
+#     num2= int(input("num2 "))
+#     print(num1+num2)
+
+#     repeat = input("Do u want to stop type Y")
+#     if repeat =="Y":
+#         break
+
+#Nested loop
+
+# i=0
+# for i in range(1,4):
+#     for j in range(1,11):
+#         print(j,end=" ")
+#     print("__ ")
+
+#reverse
+# for i in range(1,3):
+#     for j in range(10,0,-1):
+#         print(j,end="")
+#     print()
+
+
+#PATTERN
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+        
+#     print()
+
+
+#Star pattern
+# for i in range(1,11):
+#     for j in range(1,i+1):
+#         print(" * ",end="")
+#     print()
+
+#reverse pattern
+# for i in range(0,6):
+#     for j in range(6,i,-1):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,4):
+#     for j in range(4,i,-1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(1,6):
+#     for j in range()
