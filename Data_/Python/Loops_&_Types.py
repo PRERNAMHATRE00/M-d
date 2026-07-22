46676858fcee2617573411f2664168f41141b135
Prerna <mhatreprerna800@gmail.com>
2026-07-22 17:49:32 +0530

    For loop, While loop, while True, Nested loop, for loop with condition, while loop with condition , break,continue

--- Data_/Python/Loops_&_Types.py
+++ Data_/Python/Loops_&_Types.py
@@ -72,5 +72,89 @@
 #         print("*",end=" ")
 #     print()
 
+
 # for i in range(1,6):
-#     for j in range()
+#     for j in range(1,7-i):
+#      print("*",end="")
+#     print()
+
+#Triangle
+# row=5
+# for i in range(1,row +1):
+#     print(" "*(row-i),end="")
+#     print("* "*i)
+
+#
+
+# for i in range(0,5):
+#     for j in range(0,i):
+#         print(i,end=" ")
+#     print()
+
+#Floyd's Triangle
+# num=1
+# for i in range(1,5):
+#     for j in range(1,i-1):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
+
+# num=1
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
+#Continuous Alphabet Pattern
+# for i in range(1,6):
+#     for j in range (1,i+1):
+#         print(chr(64 +j),end="")
+#     print()
+
+
+
+#for loop in condition
+# for i in range(0,12):
+#     if i <=3:
+#         print(i,"  top song")
+#     else:
+#         print(i)
+
+# for i in range(0,1000):
+#     if i % 8==0 and i % 12 ==0:
+#         print(i,end=" ")
+
+
+#While  loop in condition
+# i=0
+# while i<10:
+#     if i<=3:
+#      print(i,"Top song")
+#     else:
+#        print(i)
+#     i+=1
+
+#Break and contionue
+
+# for i in range(1,21):
+#     if i==6 or i==9:
+#         continue
+#     else:
+#         print(i,end=" ")
+
+# for i in range(1,21):
+#     if i==6 and i==9:
+#         continue
+#     else:
+#         print(i,end=" ")
+
+for i in range(1,21):
+    if i==6 or i==9:
+        break
+    else:
+        print(i,end=" ")
+        print("\nHi\n")
+
+print("Thank you")
